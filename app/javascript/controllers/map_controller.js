b9ffdf5969864164ffb4219819b0842d54453f31
Julien Sudre <julien.sudre@innotrope.com>
2025-07-08 16:02:12 +0200

    Improvement of index page design

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,40 +8,46 @@ export default class extends Controller {
   }
 
   connect() {
-
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/outdoors-v12"
     })
+
+    this.mapMarkers = [] // pour conserver la liste des markers et leurs coords
+
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.#setupCardHoverEvents()
+  }
 
-    // Écouter les événements de recherche
-    document.addEventListener("map:center", (event) => {
-      this.#centerMap(event.detail.lat, event.detail.lng)
+  #addMarkersToMap() {
+    this.markersValue.forEach((markerData) => {
+      const popup = new mapboxgl.Popup().setHTML(markerData.info_window_html)
+
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = markerData.marker_html
+      customMarker.classList.add("custom-marker")
+
+      const marker = new mapboxgl.Marker(customMarker)
+        .setLngLat([markerData.lng, markerData.lat])
+        .setPopup(popup)
+        .addTo(this.map)
+
+      // On stocke le marker + DOM + coords
+      this.mapMarkers.push({
+        marker: marker,
+        element: customMarker,
+        lat: markerData.lat,
+        lng: markerData.lng
+      })
     })
   }
 
-#addMarkersToMap() {
-  this.markersValue.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-
-    // Custom marker avec badge prix
-    const customMarker = document.createElement("div")
-    customMarker.innerHTML = marker.marker_html
-
-    new mapboxgl.Marker(customMarker)
-      .setLngLat([marker.lng, marker.lat])
-      .setPopup(popup)
-      .addTo(this.map)
-  })
-}
-
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 
@@ -49,7 +55,32 @@ export default class extends Controller {
     this.map.flyTo({
       center: [lng, lat],
       zoom: 14,
-      duration: 2000
+      duration: 500
+    })
+  }
+
+  #setupCardHoverEvents() {
+    const cards = document.querySelectorAll("[data-lat][data-lng]")
+    cards.forEach((card) => {
+      card.addEventListener("mouseover", () => {
+        const lat = parseFloat(card.dataset.lat)
+        const lng = parseFloat(card.dataset.lng)
+
+        if (!isNaN(lat) && !isNaN(lng)) {
+          this.#centerMap(lat, lng)
+          this.#highlightMarker(lat, lng)
+        }
+      })
+    })
+  }
+
+  #highlightMarker(lat, lng) {
+    this.mapMarkers.forEach(({ lat: mLat, lng: mLng, element }) => {
+      if (mLat === lat && mLng === lng) {
+        element.classList.add("marker-highlight")
+      } else {
+        element.classList.remove("marker-highlight")
+      }
     })
   }
 }
